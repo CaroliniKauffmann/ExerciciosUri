ee0e66ad7ac70f9573494d5e488ca290d1018523
CaroliniKauffmann <carolini.kauffmann@gmail.com>
2021-09-08 11:31:42 -0300

    Iniciando Projeto de Estudos

---
+++ src/main/kotlin/exerciciosUri/1001.kt
@@ -0,0 +1,14 @@
+package exerciciosUri
+
+import java.util.*
+
+fun main(args: Array<String>) {
+
+    val scan = Scanner(System.`in`)
+    val valorA = scan.nextLine().toInt()
+    val valorB = scan.nextLine().toInt()
+    val valorX = valorA + valorB
+
+    println("X = $valorX")
+
+}
---
+++ src/main/kotlin/exerciciosUri/1002.kt
@@ -0,0 +1,12 @@
+package exerciciosUri
+
+import java.util.*
+
+fun main(args: Array<String>) {
+
+    val scan = Scanner(System.`in`)
+    val raio = Double
+    val area = Double
+
+
+}

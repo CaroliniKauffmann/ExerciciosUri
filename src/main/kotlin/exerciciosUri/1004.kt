999509080600d513fcaefc094a0120edffe6d853
CaroliniKauffmann <carolini.kauffmann@gmail.com>
2021-09-09 01:05:58 -0300

    Exercicio - Produto Simples

---
+++ src/main/kotlin/exerciciosUri/1004.kt
@@ -0,0 +1,13 @@
+package exerciciosUri
+
+import java.util.*
+
+fun main() {
+
+    val scan = Scanner(System.`in`)
+    val valor1 = scan.nextLine().toInt()
+    val valor2 = scan.nextLine().toInt()
+    val prod = valor1 * valor2
+
+    println("PROD = $prod")
+}

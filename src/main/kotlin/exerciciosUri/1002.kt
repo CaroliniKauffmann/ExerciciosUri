096ce1225c8b1d71486c39176fdc2307c0f55689
CaroliniKauffmann <carolini.kauffmann@gmail.com>
2021-09-09 00:37:03 -0300

    Exercicio - Área do Círculo

--- src/main/kotlin/exerciciosUri/1002.kt
+++ src/main/kotlin/exerciciosUri/1002.kt
@@ -1,12 +1,15 @@
 package exerciciosUri
 
 import java.util.*
+import kotlin.math.PI
+import kotlin.math.pow
 
 fun main(args: Array<String>) {
 
     val scan = Scanner(System.`in`)
-    val raio = Double
-    val area = Double
+    val raio = scan.nextLine().toDouble()
+    val area = PI * raio.pow(2)
+    val formatarea = String.format("%.4f", area)
 
-
+    println("A=$formatarea")
 }

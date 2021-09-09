6a788adeb1220d5fb517ad912a92815836fc5d01
CaroliniKauffmann <carolini.kauffmann@gmail.com>
2021-09-09 01:00:28 -0300

    Exercicio - Soma Simples

---
+++ src/main/kotlin/exerciciosUri/1003.kt
@@ -0,0 +1,14 @@
+package exerciciosUri
+
+import java.util.*
+
+fun main() {
+
+    val scan = Scanner(System.`in`)
+    val valorA = scan.nextLine().toInt()
+    val valorB = scan.nextLine().toInt()
+    val soma = valorA + valorB
+
+    println("SOMA = $soma")
+
+}
